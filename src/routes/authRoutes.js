43cb1f8cbc8b4731c2fb38a4889930a0010ff122
Ardhii <127921311+diefordie@users.noreply.github.com>
2024-12-23 17:45:40 +0800

    revisi auth dan edit profile

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import { registrasi, login, logout, forgotPassword, resetPasswordController } from '../controllers/authController.js';
+import { validateRequest } from '../middleware/authMiddleware.js';
+import { registerValidation } from '../validator/auth.js';
 
 const router = express.Router();
 
+
 // Route untuk registrasi
-router.post('/registrasi', registrasi);
+router.post('/registrasi', registerValidation, validateRequest, registrasi);
 
 // Route untuk login
 router.post('/login', login);
 
 // Route untuk logout
 router.post('/logout', logout);
+
+
 
 
 router.post('/forgot-password', forgotPassword);
