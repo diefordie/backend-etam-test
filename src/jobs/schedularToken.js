60e7474a502360725d59f55dd06c609d9dd503ae
Ardhii <127921311+diefordie@users.noreply.github.com>
2025-01-01 10:54:26 +0800

    update be

--- src/jobs/schedularToken.js
+++ src/jobs/schedularToken.js
@@ -1,11 +1,9 @@
 import { cleanupRevokedTokens } from '../services/auth/logout.js'; // Adjust the import
 import cron from 'node-cron'; 
 
-// Set up a cron job to run every hour
 export const startCleanupJob = () => {
-    // Set up a cron job to run every hour
-    cron.schedule('0 * * * *', () => {
+    cron.schedule('0 0 * * *', () => {
         console.log('Running cleanup of revoked tokens...');
         cleanupRevokedTokens();
-    });
+    });
 };
